eda5d4098cda2ce9a7aca1e8482d0ecb8a899e25
admin <986467599@qq.com>
2020-07-09 23:09:52 +0800

    错误修改

--- serviceWorker.js
+++ serviceWorker.js
@@ -14,7 +14,7 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('fetch', e => {
-	e.responseWith(caches.match(e.request)).then(response => {
+	e.respondWith(caches.match(e.request)).then(response => {
 		if (response !== undefined) {
 			return response;
 		} else {
